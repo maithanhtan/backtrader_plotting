c7e1bbe9a5b338dba5098e6f0f24749b349290a3
maithanhtan <maithanhtanbl@gmail.com>
2020-06-24 15:04:02 +0700

    Update metadata.py

--- backtrader_plotting/html/metadata.py
+++ backtrader_plotting/html/metadata.py
@@ -66,7 +66,7 @@ def _get_strategy(strategy: bt.Strategy) -> str:
         md += _get_parameter_table(i.params)
 
     md += 'Source Code:\n'
-    md += f'\n```\n{inspect.getsource(strategy.__class__)}\n```\n\n'
+    md += ''
 
     return md
 
